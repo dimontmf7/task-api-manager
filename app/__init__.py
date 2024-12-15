1e1a101a64f1b1b46209ac3d4a735310e25141ef
Dmitriy Timofeev <d.i.timofeev@mail.ru>
2024-12-15 23:36:27 +0300

    add secrets

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'your-secret-key'  # Замените на случайное значение
+    app.config['JWT_SECRET_KEY'] = 'c0a156954253f90bf7ef2cbc9b21185abf8ee7fccb95da1d0c23c0be67739100'  
 
     db.init_app(app)
     api.init_app(app)
